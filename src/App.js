9dcf70667126b0ceeae6e4f117b519c45a2a2c48
Vicente Spencer Noriega Moreno <vicente.noriega.sc@gmail.com>
2023-08-29 01:13:22 -0600

    Map loteria cards

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
 import { useState } from "react";
 import { getLoteriaRand } from "utils/random";
+import { loteriaMap } from "utils/loteria";
 
 function App() {
-  const [name, setName] = useState("");
   const [randomCard, setRandomCard] = useState(`00`);
 
-  const onSubmitHandler = () => {};
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+    // TODO: store the name and the card
+    const cartaLoteria = getLoteriaRand();
+    setRandomCard(cartaLoteria);
+    console.log(
+      "🚀 ~ file: App.js:10 ~ onSubmitHandler ~ e:",
+      e.target.name.value,
+      loteriaMap[cartaLoteria]
+    );
+  };
 
   return (
     <div className="App">
       <h1 className="Title">Loteria</h1>
       <div className="ImageContainer">
-        <img className="fullWidth" src={`/images/Cartas-${randomCard}.png`} />
+        <img
+          className="fullWidth"
+          src={`/images/Cartas-${randomCard}.png`}
+          alt="Carta de loteria"
+        />
       </div>
-      <form className="Form">
+      <form className="Form" onSubmit={onSubmitHandler}>
         <input
           className="InputField"
-          type="text"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
           placeholder="Nombre participante"
+          name="name"
+          type="text"
         />
         <input className="SubmitButton" type="submit" value="Generar" />
       </form>
